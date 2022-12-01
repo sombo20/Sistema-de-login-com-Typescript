ed90780954ebe14bd2729415857d21531ffc61f5
VICENTE <64757262+sombo20@users.noreply.github.com>
2022-12-01 12:05:16 +0100

    Update index.tsx

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -9,42 +9,60 @@ function Signup(){
   const navigate = useNavigate()
   
   const [email,setEmail] = useState<string>("")
-  const [senha,setSenha] = useState<string>("")
+  const [password,setPassword] = useState<string>("")
   const [error,setError] = useState<string>("")
   
-  const handlesignin = ()=>{
-     if(!email | !senha){
+  const handleSignup = (e)=>{
+    e.preventDefault()
+     if(!email | !password){
        setError("Input empty")
        return
      }
      
-     const res = signup(email,senha)
+     const res = signup(email,password)
      if(res){
        setError(res)
        return
      }
-     alert("Usuario cadastrado")
+     alert("User Registed")
      navigate("/")
   }
     
   return(
      <>
-     <h1>Signup</h1>
-     <Input
+   <form onSubmit={handleSignup}>
+	<div class="container" >
+			<div class="text" >
+				<span><h2>Register</h2></span>
+			</div>
+			<div class="inputs">
+			  <div class="item">
+				<label>Email</label>
+			  <Input
        type="email"
        placeholder="Set your email.."
        value={email}
        onchange={(e)=>[setEmail(e.target.value),setError("")]}/>
-         <Input
+			  </div>
+			  <div class="item">
+			    <label>Password</label>
+			             <Input
        type="password"
-       placeholder="Set your senha..."
-       value={senha}
-       onchange={(e)=>[setSenha(e.target.value),setError("")]}/>
-     <Button
-      Text="Signin"
-      onclick={handlesignin}
+       placeholder="Set your password..."
+       value={password}
+       onchange={(e)=>[setPassword(e.target.value),setError("")]}/>
+       <p>{error}</p>
+			  </div>
+			</div>
+			<div class="btn">
+				<Button
+      text="Signup"
+      onclick={handleSignup}
     />
-    <p>{error}</p>
+			</div>
+		</div>
+   
+    </form>
     </>
   )
 }
